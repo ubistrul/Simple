c214f6226c2db86e8f86ed87d05e537da8466433
Alexey Serduchenko <ubistrul@gmail.com>
2023-11-17 20:00:23 +0200

    First

---
+++ js/main.js
@@ -0,0 +1,44 @@
+const body = document.querySelector('body');
+
+const searchBtnBottom = document.querySelector('.nav-buttons-bottom__search');
+const searchFormBottom = document.querySelector('.search-form-bottom');
+let searchBtnBottomAriaExpanded = searchFormBottom.getAttribute('aria-expanded');
+const searchFormBottomActive = 'search-form-bottom--width';
+const searchCloseBtnBottom = document.querySelector('.search-form-bottom__close');
+
+const burgerMenuBtn = document.querySelector('.burger-menu');
+const burgerMenuClass = 'burger-menu--active';
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuClass = 'mobile-menu--active';
+
+burgerMenuBtn.addEventListener('click', () => {
+        burgerMenuBtn.classList.toggle(burgerMenuClass);
+        mobileMenu.classList.toggle(mobileMenuClass);
+        body.classList.toggle('no-scroll')
+})
+
+console.log(searchBtnBottomAriaExpanded);
+
+searchBtnBottom.addEventListener('click', () => {
+    if (!searchFormBottom.classList.contains(searchFormBottomActive)) {
+        searchBottomOpen();
+    }
+})
+
+
+searchCloseBtnBottom.addEventListener('click', () => {
+    if (searchFormBottom.classList.contains(searchFormBottomActive)) {
+        searchBottomClose();
+    }
+});
+
+function searchBottomOpen () {
+    searchFormBottom.classList.add(searchFormBottomActive);
+    searchFormBottom.setAttribute('aria-expanded', true);
+}
+
+function searchBottomClose () {
+
+    searchFormBottom.classList.remove(searchFormBottomActive);
+    searchFormBottom.setAttribute('aria-expanded', false);
+}
