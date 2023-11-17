8cc24ba407969145c5db2475deda3e9972521d2d
Alexey Serduchenko <ubistrul@gmail.com>
2023-11-17 21:47:49 +0200

    Update JS code
    Mini hamburger menu behavior fix

--- js/main.js
+++ js/main.js
@@ -10,11 +10,20 @@ const burgerMenuBtn = document.querySelector('.burger-menu');
 const burgerMenuClass = 'burger-menu--active';
 const mobileMenu = document.querySelector('.mobile-menu');
 const mobileMenuClass = 'mobile-menu--active';
+const mobileMenuLink = document.querySelector('.mobile-menu__nav-link')
 
 burgerMenuBtn.addEventListener('click', () => {
         burgerMenuBtn.classList.toggle(burgerMenuClass);
         mobileMenu.classList.toggle(mobileMenuClass);
         body.classList.toggle('no-scroll')
+})
+
+mobileMenu.addEventListener('click', (e) => {
+    if (e.target != mobileMenuLink) {
+        burgerMenuBtn.classList.remove(burgerMenuClass);
+        mobileMenu.classList.remove(mobileMenuClass)
+        body.classList.remove('no-scroll')
+    }
 })
 
 console.log(searchBtnBottomAriaExpanded);
@@ -38,7 +47,6 @@ function searchBottomOpen () {
 }
 
 function searchBottomClose () {
-
     searchFormBottom.classList.remove(searchFormBottomActive);
     searchFormBottom.setAttribute('aria-expanded', false);
 }
